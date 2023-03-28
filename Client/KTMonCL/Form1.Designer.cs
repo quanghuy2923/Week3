754a4137be7937171ddc7f2bd21b22d01f270888
quanghuy2923 <mrzxkun@gmail.com>
2023-03-28 19:28:00 +0700

    first commit

---
+++ Client/KTMonCL/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace KTMonCL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbPort = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbHienThi = new System.Windows.Forms.ListBox();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.btnGui = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbPort
+            // 
+            this.lbPort.AutoSize = true;
+            this.lbPort.Location = new System.Drawing.Point(322, 57);
+            this.lbPort.Name = "lbPort";
+            this.lbPort.Size = new System.Drawing.Size(29, 13);
+            this.lbPort.TabIndex = 17;
+            this.lbPort.Text = "Port:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Địa chỉ IP:";
+            // 
+            // lbHienThi
+            // 
+            this.lbHienThi.FormattingEnabled = true;
+            this.lbHienThi.HorizontalScrollbar = true;
+            this.lbHienThi.Location = new System.Drawing.Point(35, 86);
+            this.lbHienThi.Name = "lbHienThi";
+            this.lbHienThi.Size = new System.Drawing.Size(456, 212);
+            this.lbHienThi.TabIndex = 15;
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(35, 304);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(365, 20);
+            this.txtSend.TabIndex = 12;
+            this.txtSend.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSend_KeyDown);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(357, 54);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(134, 20);
+            this.txtPort.TabIndex = 10;
+            this.txtPort.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPort_KeyDown);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(94, 54);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(198, 20);
+            this.txtIP.TabIndex = 9;
+            // 
+            // btnGui
+            // 
+            this.btnGui.Location = new System.Drawing.Point(406, 304);
+            this.btnGui.Name = "btnGui";
+            this.btnGui.Size = new System.Drawing.Size(85, 20);
+            this.btnGui.TabIndex = 13;
+            this.btnGui.Text = "Gửi";
+            this.btnGui.UseVisualStyleBackColor = true;
+            this.btnGui.Click += new System.EventHandler(this.btnGui_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(35, 16);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(125, 32);
+            this.btnConnect.TabIndex = 11;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(366, 16);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(125, 32);
+            this.btnDisconnect.TabIndex = 14;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 339);
+            this.Controls.Add(this.lbPort);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbHienThi);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.btnGui);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnDisconnect);
+            this.Name = "Form1";
+            this.Text = "Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lbHienThi;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Button btnGui;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+    }
+}
+
---
+++ Sever/KTMonSV/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace KTMonSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNhap = new System.Windows.Forms.TextBox();
+            this.lbHienThi = new System.Windows.Forms.ListBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnGui = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNhap
+            // 
+            this.txtNhap.Location = new System.Drawing.Point(27, 306);
+            this.txtNhap.Name = "txtNhap";
+            this.txtNhap.Size = new System.Drawing.Size(381, 20);
+            this.txtNhap.TabIndex = 6;
+            this.txtNhap.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtNhap_KeyDown);
+            // 
+            // lbHienThi
+            // 
+            this.lbHienThi.FormattingEnabled = true;
+            this.lbHienThi.HorizontalScrollbar = true;
+            this.lbHienThi.Location = new System.Drawing.Point(27, 49);
+            this.lbHienThi.Name = "lbHienThi";
+            this.lbHienThi.Size = new System.Drawing.Size(475, 251);
+            this.lbHienThi.TabIndex = 9;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(429, 9);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(73, 34);
+            this.btnStop.TabIndex = 8;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnGui
+            // 
+            this.btnGui.Location = new System.Drawing.Point(414, 305);
+            this.btnGui.Name = "btnGui";
+            this.btnGui.Size = new System.Drawing.Size(88, 20);
+            this.btnGui.TabIndex = 7;
+            this.btnGui.Text = "Gửi";
+            this.btnGui.UseVisualStyleBackColor = true;
+            this.btnGui.Click += new System.EventHandler(this.btnGui_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(27, 9);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(73, 34);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 339);
+            this.Controls.Add(this.txtNhap);
+            this.Controls.Add(this.lbHienThi);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnGui);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNhap;
+        private System.Windows.Forms.ListBox lbHienThi;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnGui;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
